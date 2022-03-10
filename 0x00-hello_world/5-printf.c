32480e98ff1cfe01e6bd2217e4ce855e850f3c4b
The-CodeINN <emijere.richard@gmail.com>
2022-03-10 14:46:56 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase with proper grammar
+ * Return: 0 if exited successfully
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
