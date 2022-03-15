b9f42ff9d032252f3fdd2750c8d09c23384615db
The-CodeINN <emijere.richard@gmail.com>
2022-03-15 14:56:22 -0700

    Updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,22 +2,20 @@
 
 /**
  * main - prints _putchar
- *
- * Description: prints _putchar
- *
+ * Description - executes main
  * Return: 0 (Success)
  */
 
 int main(void)
 {
-    char text[9] = "_putchar";
-    int i = 0;
+	char text[9] = "_putchar";
+	int i = 0;
 
-    for (i = 0; i < 8; i++)
-    {
-        _putchar(text[i]);
-    }
-    _putchar('\n');
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
 
-    return (0);
+	return (0);
 }
